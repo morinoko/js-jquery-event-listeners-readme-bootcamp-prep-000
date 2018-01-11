f78803a9f2932016ba96f66e1d6ff3ec40c6e02c
Felice Forby <feliceforby@gmail.com>
2018-01-11 04:18:18 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $(document).on('load', function() {
+    let img = $('img');
+    img.addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('input#typing').on('keydown', function(key) {
+    if (key.which === 71 ) {
+      alert('You pressed the G key!');
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.");
+  });
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+// call functions when page loads
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
