519d8cbee25d1f6c202e486c3a8af43e50f905d5
Felice Forby <feliceforby@gmail.com>
2018-01-11 04:14:55 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,28 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $(document).on('load', function() {
+    let img = $('img');
+    img.addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('input#typing').on('keydown', function(key) {
+    if (key.which === 71 ) {
+      alert('You pressed the G key!');
+    }
+  });
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+// call functions when page loads
+  getIt();
+  frameIt();
+  pressIt();
 });
